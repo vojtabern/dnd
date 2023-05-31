ed3d9756e17c1e38ee4d19c8c2cedb5c978329eb
vojta <vojtabern@gmail.com>
2023-05-31 16:20:49 +0200

    provizorne_dodelan_index

--- mysite/dnd/static/js/scripts.js
+++ mysite/dnd/static/js/scripts.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function() {
   $("#generateStatsBtn").click(function() {
     var statsResult = generateStats(); // Call the function to generate stats
@@ -77,13 +76,18 @@ $(document).ready(function() {
     // var classes = ["Barbar", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
     // var races = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"];
 
-    var classes = ["Barbar", "Bard", "Klerik", "Druid", "Bojovník", "Mnich", "Paladin", "Hraničář", "Zloděj", "Čaroděj", "Černokěžník", "Kouzelník"];
+    var classes = ["Barbar", "Bard", "Klerik", "Druid", "Bojovník", "Mnich", "Paladin", "Hraničář", "Zloděj", "Čaroděj", "Černokěžník", "Kouzelník", "Tulák"];
     var races = ["Trpaslík", "Elf", "Halfling", "Člověk", "Drakorozený", "Gnóm", "Půl-elf", "Půl-orc", "Tiefling"];
     // Function to generate random class and race
     function generateResults() {
         var randomClass = classes[Math.floor(Math.random() * classes.length)];
         var randomRace = races[Math.floor(Math.random() * races.length)];
-        return "Povolání: " + randomClass + "<br>Rasa: " + randomRace;
+        var resultHTML = "<h1 class='display-4'>Povolání a Rasa</h1>";
+        resultHTML += "<hr><div class='row'>";
+        resultHTML += "<div class='col-md-6'><p>Povolání: <b>" + randomClass + "</b></p><p> Rasa: <b>" + randomRace +"</b></p></div>";
+        resultHTML += "<div class='col-md-6'><img src='./static/img/race_class/" + randomClass.toLowerCase() + ".jpg' width='100%' alt='povolani'></div>";
+        resultHTML += "</div>";
+        return resultHTML;
     }
 
     // Event handler for button click
@@ -92,3 +96,46 @@ $(document).ready(function() {
         $("#raceResult").html(results);
     });
 });
+
+
+$(document).ready(function() {
+  var previousRoll = null; // Variable to store the previous roll result
+
+  $('#throwDiceBtn').click(function() {
+    var diceType = parseInt($('#diceType').val());
+    var diceCount = parseInt($('#diceCount').val());
+    var throwResults = [];
+    var sumResult = 0;
+
+    // Generate throw results
+    for (var i = 0; i < diceCount; i++) {
+      var throwValue = Math.floor(Math.random() * diceType) + 1;
+      throwResults.push(throwValue);
+      sumResult += throwValue;
+    }
+
+    // Update previous roll
+    previousRoll = {
+      throwResults: throwResults,
+      sumResult: sumResult,
+        dice: diceType
+    };
+
+    // Show popup with results
+
+    // Update previous roll table
+    updatePreviousRollTable(previousRoll);
+  });
+
+  // Update previous roll table function
+  function updatePreviousRollTable(roll) {
+    var tableHTML = '<table class="table table-bordered table-striped text-center"><tr><th>Kostka</th><th>Výsledky hodů</th><th>Sečtení výsledků hodů</th></tr>';
+    tableHTML += '<tr><td>' + roll.dice + '-stranná </td><td class="text-danger text-center">' + roll.throwResults.join(', ') + '</td><td class="text-center">=' + roll.sumResult + '</td></tr></table>';
+    $('#previousRollTable').html(tableHTML);
+  }
+});
+
+
+
+
+
